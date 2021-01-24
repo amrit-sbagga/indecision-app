487e822e1c23f25bf11d1d673e18a17d15477e1e
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-24 17:55:44 +0530

    use of let, var and const

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,14 +1,12 @@
-"use strict";
-
 var nameVar = "Amrit";
 var nameVar = "Mike";
 console.log('nameVar', nameVar);
 
-var nameLet = "Jen";
+let  nameLet = "Jen";
 //let nameLet = 'Julie'
 console.log('nameLet = ', nameLet);
 
-var nameConst = "Frank";
+const nameConst = "Frank";
 console.log('nameConst = ', nameConst);
 //const nameConst ="Gunther";
 //console.log('nameConst = ', nameConst);
