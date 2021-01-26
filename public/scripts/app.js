919dca5e983ec5d43008dfb62caffa0b391346bb
Amrit Singh <amrit.sbagga@gmail.com>
2021-01-26 23:42:59 +0530

    arrow function usage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,35 @@
 "use strict";
 
-var square = function square(x) {
-    return x * x;
+//arguments object - no longer bound with arrow functions
+console.log("arrow functions usage...!!");
+
+//const add = function (a, b){
+var add = function add(a, b) {
+    // console.log(arguments);
+    return a + b;
 };
 
-var squareArrow = function squareArrow(x) {
-    return x * x;
+console.log(add(4, 5, 1001));
+
+//this keyword - no longer bound
+var user = {
+    "name": "Amrit",
+    "cities": ["Sarangarh", "Raipur", "Bhilai", "Bangalore"],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        var that = this;
+        console.log(this.name);
+        console.log(this.cities);
+
+        // this.cities.forEach(function (city){
+        //     console.log(that.name + "has lived in city = ", city);
+        // })
+
+        this.cities.forEach(function (city) {
+            console.log(_this.name + " has lived in city = ", city);
+        });
+    }
 };
 
-console.log(square(8));
-
-console.log(squareArrow(5));
-
-var getFirstName = function getFirstName(fullname) {
-    return fullname.split(" ")[0];
-};
-
-console.log(getFirstName("Amrit Singh"));
+user.printPlacesLived();
---
+++ src/playground/es6-arrow-function2.js
@@ -0,0 +1,31 @@
+//arguments object - no longer bound with arrow functions
+console.log("arrow functions usage...!!")
+
+//const add = function (a, b){
+const add = (a, b) => {
+   // console.log(arguments);
+    return a + b;
+}
+
+console.log(add(4, 5, 1001));
+
+//this keyword - no longer bound
+const user = {
+    "name" : "Amrit",
+    "cities" : ["Sarangarh", "Raipur", "Bhilai", "Bangalore"],
+    printPlacesLived: function(){
+        const that = this;
+        console.log(this.name);
+        console.log(this.cities);
+
+        // this.cities.forEach(function (city){
+        //     console.log(that.name + "has lived in city = ", city);
+        // })
+
+        this.cities.forEach((city) => {
+            console.log(this.name + " has lived in city = ", city);
+        })
+    }
+}
+
+user.printPlacesLived();
